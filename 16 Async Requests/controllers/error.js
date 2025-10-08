151e6ad56ad04fa79d0c46b1039b08b779b63574
Sherif <ibrahimsherifdeji@gmail.com>
2025-10-08 04:53:04 +0100

    async requests

---
+++ 16 Async Requests/controllers/error.js
@@ -0,0 +1,16 @@
+const isAuth = require("../middleware/is-auth");
+
+exports.get404 = (req, res, next) => {
+  res.status(404).render("404", {
+    pageTitle: "Page Not Found",
+    path: "/404",
+  });
+};
+
+exports.get500 = (req, res, next) => {
+  res.status(500).render("500", {
+    pageTitle: "Error Occurred",
+    path: "/500",
+    isAuth: req.session.isLoggedIn,
+  });
+};
