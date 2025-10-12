93aec6bd669e3395ae7bc31642b02052e8254383
Sherif <ibrahimsherifdeji@gmail.com>
2025-10-12 04:46:58 +0100

    rest apis

---
+++ 19 Working With RestApi/backend/app.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const feedRoutes = require("./routes/feed");
+
+const app = express();
+app.use(bodyParser.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/feed", feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message || "An unknown error occurred!";
+  res.status(status).json({ message: message });
+});
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((result) => {
+    app.listen(8080);
+    console.log("Connected to MongoDB and server is running on port 8080");
+  })
+  .catch((err) => console.log(err));
