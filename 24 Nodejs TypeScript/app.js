6ae78e8520b5a885bd019653af0789233531f837
Sherif Ibrahim <ibrahimsherifdeji@gmail.com>
2025-11-17 10:23:55 +0100

    nodejs-typescript

---
+++ 24 Nodejs TypeScript/app.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const num1Element = document.getElementById('num1');
+const num2Element = document.getElementById('num2');
+const buttonElement = document.querySelector('button');
+function add(num1, num2) {
+    return num1 + num2;
+}
+buttonElement.addEventListener('click', () => {
+    const num1 = num1Element.value;
+    const num2 = num2Element.value;
+    const result = add(+num1, +num2);
+    console.log(result);
+});
+//# sourceMappingURL=app.js.map
